f7053b4e7e3f6105fb77c2af3e8d94cc8333ef30
Duncan Campbell <duncan.campbell@rabobank.nl>
2020-07-02 20:55:08 +0200

    order

--- src/main/java/nl/sourcelabs/workshop/testing/application/database/EmployeeRepository.java
+++ src/main/java/nl/sourcelabs/workshop/testing/application/database/EmployeeRepository.java
@@ -9,9 +9,9 @@ import org.springframework.data.repository.PagingAndSortingRepository;
 
 public interface EmployeeRepository extends PagingAndSortingRepository<Employee, UUID> {
 
+    List<Employee> findByFirstNameIsLike(String firstNameLike);
+
     @Query(value = "select e from Employee e where e.firstName = 'Duncan' and e.lastName = 'Campbell'",
     nativeQuery = false) //set this to true to run native SQL, keep dialects in mind
     Optional<Employee> findTheBestEmployee();
-
-    List<Employee> findByFirstNameIsLike(String firstNameLike);
 }
